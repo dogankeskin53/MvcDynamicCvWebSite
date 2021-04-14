b06f17541af469b939ac31c52682cdeee39cf6ec
Doğan Keskin <dogankeskin53@gmail.com>
2021-04-14 18:42:05 +0300

    Temanın eklenmesi ve ilk değerlerin atanma işlemleri

---
+++ MvcDynamicCvWebSite/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//model çağırma
+using MvcDynamicCvWebSite.Models.Entity;
+
+namespace MvcDynamicCvWebSite.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // DB Nesnesi içerisindeki sınıflara erişim sağlanabilir.
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.About.ToList();
+            return View(degerler);
+        }
+
+        public PartialViewResult Deneyim()
+        {
+            return PartialView();
+        }
+
+
+    }
+}
---
+++ MvcDynamicCvWebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcDynamicCvWebSite.MvcApplication" Language="C#" %>
