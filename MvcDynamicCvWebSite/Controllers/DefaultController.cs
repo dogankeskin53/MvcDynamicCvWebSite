7a8f0279946eea1c6e4e9e0d1e8a83153ca64c54
dogankeskin53 <dogankeskin53@gmail.com>
2021-04-16 03:32:55 +0300

    Admin Lte eklenmesi

--- MvcDynamicCvWebSite/Controllers/DefaultController.cs
+++ MvcDynamicCvWebSite/Controllers/DefaultController.cs
@@ -12,14 +12,61 @@ namespace MvcDynamicCvWebSite.Controllers
     {
         // DB Nesnesi içerisindeki sınıflara erişim sağlanabilir.
         DbCvEntities db = new DbCvEntities();
+       
         public ActionResult Index()
         {
             var degerler = db.About.ToList();
             return View(degerler);
         }
 
-        public PartialViewResult Deneyim()
+        public PartialViewResult Experience()
         {
+            var experience = db.Experience.ToList();
+            return PartialView(experience);
+        }
+
+        public PartialViewResult Education()
+        {
+            var education = db.Education.ToList();
+            return PartialView(education);
+        }
+
+        public PartialViewResult MySkills()
+        {
+            var mySkills = db.MySkils.ToList();
+            return PartialView(mySkills);
+        }
+
+        public PartialViewResult Interests()
+        {
+            var interests = db.Interests.ToList();
+            return PartialView(interests);
+        }
+
+        public PartialViewResult Awards()
+        {
+            var awards = db.Awards.ToList();
+            return PartialView(awards);
+        }
+
+        [HttpGet]
+        public PartialViewResult Contact()
+        {
+            return PartialView();
+        }
+
+        /// <summary>
+        /// Butona tıklayınca post metodu çağrılacak
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public PartialViewResult Contact(Contact t)
+        {
+            ///Hangi bilgi gelirse gelsin t türünde alıp kayıt altına alıyoruz
+            ///SaveChanges metodu ile kayıt ediyoruz.
+            db.Contact.Add(t);
+            db.SaveChanges();
             return PartialView();
         }
 
